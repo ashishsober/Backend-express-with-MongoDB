97095a260bbb9c0427211771f3cef4344ae848cd
Ashish Gupta <ashishguptawaiting@gmail.com>
2017-11-05 05:15:53 +0530

    get tripSummary and post trip summary working

--- app/controllers/myServices.Controller.js
+++ app/controllers/myServices.Controller.js
@@ -1,25 +1,39 @@
 var dbobj = require('./db');
 //var crypto = require('crypto');
 var User = require('../schemas/userSchema');
+var Trip = require('../schemas/tripSchema');
 
 
-    exports.postTripSummary = function(req, res){
-         var post=req.body;
-		     var db =dbobj.db;
-         db.collection('tripSummary').save(post, function(err, result){
-		        if (err) 
-              return console.log(err)
+     exports.postTripSummary = function(req, res){
+            var newTrip = new Trip();
+            newTrip.truckNo=req.body.truckNo;
+            newTrip.status=req.body.status;
+           
+            newTrip.tripDetails.from=req.body.tripDetails.from;
+            newTrip.tripDetails.to=req.body.tripDetails.to;
+            newTrip.tripDetails.startDate=req.body.tripDetails.startDate;
+            newTrip.tripDetails.endDate=req.body.tripDetails.endDate;
+
+            newTrip.partyDetails.soldToParty=req.body.partyDetails.soldToParty;
+            newTrip.partyDetails.contactDetails=req.body.partyDetails.contactDetails;
+            newTrip.partyDetails.amount=req.body.partyDetails.amount;
+            newTrip.partyDetails.paid=req.body.partyDetails.paid; 
+
+         newTrip.save(function(err, result){
+            if (err) 
+               return console.log(err)
               res.status(200);
               res.json({ message: 'Saved to database successfully' });
-		     })
+          
+         })
+     
     };
 
 
 
     
     exports.getTripSummary = function(req, res){
-        var db =dbobj.db;
-        db.collection('tripSummary').find().toArray(function(err,results){
+       Trip.find(function(err,results){
             res.set('Content-Type', 'application/json');
             res.send(results);
         })
--- app/schemas/tripSchema.js
+++ app/schemas/tripSchema.js
@@ -3,7 +3,6 @@ var mongoose= require('mongoose');
 var tripSchema = new mongoose.Schema({
 							truckNo:{ 
 								        type: String,
-								        unique:true,
 								        required:true,
 								        trim:true
 								       },
@@ -11,21 +10,21 @@ var tripSchema = new mongoose.Schema({
 								        type: String,
 								        required:true,
 								        trim:true
-								     },
-							tripDetails:{ 
-								         from: {type:String,required:true},
-								         to: {type:String,required:true},
-								         startDate: {type:String,required:true},
-								         endDate: {type:String,required:true}
-						                },
-						    partyDetails:{
+								  },
+							tripDetails: {
+								    from: { type: String,required:true  },
+								    to:  { type: String,required:true  },
+								    startDate: {type:String,required:true},
+							        endDate: {type:String,required:true}
+								  },
+							partyDetails:{
 									    soldToParty: {type:String,required:true},
-									    contactDetails: {type:String,required:true},
-									    amount: {type:String,required:true},
+									    contactDetails: {type:Number,required:true},
+									    amount: {type:Number,required:true},
 									    paid: {type:String,required:true}
 								      }
 						 
 							});
 
-var Trip = mongoose.model('tripSummary', tripSchema);
+var Trip = mongoose.model('tripsummaries', tripSchema);
 module.exports=Trip;
