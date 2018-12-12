23bab36de4f6cde9c9e10a390ecc9b27e09aa27b
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-12 13:13:45 +0530

    created contact template

--- app/controllers/contact-vrd/contact-mail.controller.js
+++ app/controllers/contact-vrd/contact-mail.controller.js
@@ -1,26 +1,45 @@
 const nodemailer = require('nodemailer');
+const fs =require('fs');
+const handlebars = require('handlebars');
 
-exports.sendMessage = (res) => {
-    console.log("inside send email message controller"+res);
-    let mailOptions = {
-      from: 'ashishguptawaiting@gmail.com',
-      to: 'ashishguptawaiting@gmail.com',
-      subject: 'Notification from VRD-Network contact page',
-      html: '<p><b>That was easy! BOLD {{ res.phone_no}}</b></p>'
-     };
-    var transporter = nodemailer.createTransport({
+exports.sendMessage = (data) => {
+    console.log("inside send email message controller--"+data);
+    let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: 'ashishguptawaiting@gmail.com',
         pass: 'anjanibhai1@lux1'
       }
      });
-    
-     transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-          console.log(error);
-          } else {
-          console.log('Email sent: ' + info.response);
-          }
-     });
+     let readHTMLFile = function(path, callback) {
+        fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+            if (err) {
+                throw err;
+                callback(err);
+            }
+            else {
+                callback(null, html);
+            }
+        });
+    };
+     
+    readHTMLFile(__dirname + '/contact-mail.html', function(err, html) {
+        console.log("my html"+html);
+        let template = handlebars.compile(html);
+        var replacements = data;
+        var htmlToSend = template(replacements);
+        let mailOptions = {
+            from: 'ashishguptawaiting@gmail.com',
+            to: 'ashishguptawaiting@gmail.com',
+            subject: 'Notification from VRD-Network contact page',
+            html: htmlToSend
+           };
+         transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+            }
+         });
+    });
   };
