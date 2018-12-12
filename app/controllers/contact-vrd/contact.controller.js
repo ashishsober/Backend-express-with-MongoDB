72f6f7c44ed61e5c9eccab10a52add717e3b2128
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-12 12:01:36 +0530

    Update contact.controller.js

--- app/controllers/contact-vrd/contact.controller.js
+++ app/controllers/contact-vrd/contact.controller.js
@@ -10,13 +10,8 @@ let myObj = {
         "response_action": ""
     }
 };
-let mailOptions = {
-    from: 'ashishguptawaiting@gmail.com',
-    to: 'laxmi.singh887@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-};
-exports.postContact = (req, res) => {
+
+exports.postContact = (req, res, next) => {
     var newContact = new Contact();
     newContact.name = req.body.name;
     newContact.email = req.body.email;
@@ -24,55 +19,46 @@ exports.postContact = (req, res) => {
     newContact.service = req.body.service;
     newContact.other = req.body.other;
     newContact.comment = req.body.comment;
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'ashishguptawaiting@gmail.com',
-          pass: 'anjanibhai1@lux1'
-        }
-    });
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-    });
+    
     var promise = newContact.save();
     promise.then((response) => {
         myObj.applicants = response._doc;
         myObj.application.message = "Successfully Saved";
         myObj.application.response_type = "info";
         myObj.application.response_action = "continue";
-
         res.status(201);
-        res.json(myObj);
+        res.json(myObj).end();
+        sendMessage(response);
     },(error) => {
         myObj.application.message = error.message;
         myObj.application.response_type = "hard";
         myObj.application.response_action = "stop";
         res.status(400);
-        res.json(myObj);
+        res.json(myObj).end();
     });
-
-
-    // newContact.save(function (error, result) {
-    //     if (error) {
-    //         myObj.application.message = error.message;
-    //         myObj.application.response_type = "hard";
-    //         myObj.application.response_action = "stop";
-    //         res.status(400);
-    //         res.json(myObj);
-    //     } else {
-    //         myObj.applicants = result._doc;
-    //         myObj.application.message = "Successfully Saved";
-    //         myObj.application.response_type = "info";
-    //         myObj.application.response_action = "continue";
-    //         res.status(201);
-    //         res.json(myObj);
-    //     }
-
-    // });
-
 };
+
+
+function sendMessage(res){
+  let mailOptions = {
+    from: 'ashishguptawaiting@gmail.com',
+    to: 'ashishguptawaiting@gmail.com',
+    subject: 'Notification from VRD-Network contact page',
+    html: '<p><b>That was easy! BOLD</b></p>'
+   };
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'ashishguptawaiting@gmail.com',
+      pass: 'anjanibhai1@lux1'
+    }
+   });
+  
+   transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Email sent: ' + info.response);
+        }
+   });
+}
