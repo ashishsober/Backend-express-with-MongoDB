6a1188897e9ecf1fd1663e8a67da5e774e9ce0de
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-13 16:52:50 +0530

    Update career.controller.js

--- app/controllers/career-vrd/career.controller.js
+++ app/controllers/career-vrd/career.controller.js
@@ -7,15 +7,18 @@ exports.postCareer = (req, res, next) => {
     let myObj = req.body;
     if(myObj.application.stage ==='bd' && myObj.applicants.vrd_ref_number === ''){
         Career.find().count(function (err, results) {
+            console.log("my total count"+results);
             saveDataCall(results,req,res,myObj);
         });
+    } else if(myObj.application.stage ==='ad' && myObj.applicants.vrd_ref_number !== ''){
+        updateDataCall(myObj,req,res);
     } else {
-        console.log(req.body.application.vrd_ref_number);
         res.status(201);
         res.json(myObj).end();
     }
     
 };
+
 
 function saveDataCall(count,req,res, myObj){
     let newCareer = new Career(req.body.applicants);
@@ -49,3 +52,24 @@ function saveDataCall(count,req,res, myObj){
         res.json(myObj).end();
     });
 };
+
+
+function updateDataCall(myObj,req,res){
+    var myquery = {vrd_ref_number:myObj.applicants.vrd_ref_number};
+    Career.updateOne(myquery,{ $set: myObj.applicants },function(error,result){
+        if(error){
+            myObj.application.message = error.message;
+            myObj.application.response_type = "hard";
+            myObj.application.response_action = "stop";
+            res.status(400);
+            res.json(myObj).end();
+        } else {
+            console.log(result.nModified + " document(s) updated");
+            myObj.application.message = "Successfully Saved";
+            myObj.application.response_type = "info";
+            myObj.application.response_action = "continue";
+            res.status(201);
+            res.json(myObj).end();
+        }
+    });
+}
