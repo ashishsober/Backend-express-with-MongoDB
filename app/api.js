2c5c54d2f622262657f26c77de90d7b2619a5e35
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-12 00:54:50 +0530

    Update api.js

--- app/api.js
+++ app/api.js
@@ -1,14 +1,15 @@
-var myServices= require('./controllers/myServices.Controller.js');
-var contactController= require('./controllers/contact-vrd/contact.controller');
-var careerController= require('./controllers/career-vrd/career.controller');
-var employeeController= require('./controllers/employee-vrd/employee.controller');
+var myServices = require('./controllers/myServices.Controller.js');
+var contactController = require('./controllers/contact-vrd/contact.controller');
+var careerController = require('./controllers/career-vrd/career.controller');
+var employeeController = require('./controllers/employee-vrd/employee.controller');
 var google = require('./config/auth');
 const Window = require('window');
 /**
-* Routes
-*/
-module.exports = function(app,passport) {
+ * Routes
+ */
+module.exports = function (app, passport) {
 	const window = new Window();
+	app.use(passport.initialize());
 	app.post('/truck/tripSummary', myServices.postTripSummary);
 	app.get('/truck/tripSummary', myServices.getTripSummary);
 	app.get('/register/user', myServices.getUser);
@@ -21,6 +22,19 @@ module.exports = function(app,passport) {
 	app.post('/application/auth', employeeController.authEmployee);
 	app.post('/application/logout', employeeController.logout);
 
-	app.get('/auth/google',employeeController.googleAuth);
-    app.get('/auth/google/callback',employeeController.googleAuthCallback);
+	app.get('/auth/google', employeeController.googleAuth);
+	//app.get('/auth/google/callback', employeeController.googleAuthCallback);
+	app.get('/auth/google/callback',
+		passport.authenticate('google', {
+			failureRedirect: '/auth/fail'
+		}),
+		function (req, res) {
+			var responseHTML = '<html><head><title>Main</title></head><body></body><script>res = %value%; window.opener.postMessage(res, "*");window.close();</script></html>'
+			responseHTML = responseHTML.replace('%value%', JSON.stringify({
+				user: req.user
+			}));
+			res.status(200).send(responseHTML);
+
+
+		});
 }
