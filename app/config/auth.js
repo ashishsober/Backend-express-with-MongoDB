4d95b446593f5ffff5d4bc2d6fad15109a2c9c7a
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-15 00:29:51 +0530

    configuration changes

--- app/config/auth.js
+++ app/config/auth.js
@@ -8,7 +8,27 @@ const defaultScope = [
   'https://www.googleapis.com/auth/userinfo.email',
 ];
 
-module.exports = {
+exports.auth = {
   'googleAuth' : googleConfig,
   'defaultScope':defaultScope
 };
+
+
+//mongo db urls
+//configuration
+exports.db_urls = {
+	'url' : 'mongodb://localhost:27017/data',
+	'prod_url':'mongodb://test:test@ds145128.mlab.com:45128/myappdatabase12'
+};
+
+
+// "mongodb": {
+//     "host": "ds145128.mlab.com",
+//     "port": 45128,
+//     "url": "",
+//     "database": "myappdatabase12",
+//     "password": "test",
+//     "name": "mongodb",
+//     "user": "test",
+//     "connector": "mongodb"
+//   }
--- app/config/passport.js
+++ app/config/passport.js
@@ -3,10 +3,11 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var configAuth = require('./auth');
 var empDatabase = require('../controllers/employee-vrd/employee.controller');
+
 passport.use(new GoogleStrategy({
-        clientID        : configAuth.googleAuth.clientId,
-        clientSecret    : configAuth.googleAuth.clientSecret,
-        callbackURL     : configAuth.googleAuth.redirect
+        clientID        : configAuth.auth.googleAuth.clientId,
+        clientSecret    : configAuth.auth.googleAuth.clientSecret,
+        callbackURL     : configAuth.auth.googleAuth.redirect
         },(token, refreshToken, profile, done) => {
             console.log("my token data-----------"+token);
             profile.accessToken = token;
