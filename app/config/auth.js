b2e25250e127c573eadb16faa1c1ab78b0944f7f
Ashish Gupta <ashish.gupta1@sc.com>
2019-03-31 00:39:36 +0530

    console.log

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,7 +1,7 @@
 const googleConfig = {
   clientId: '103586939904-gv4s08qapffhkjfoh568jsuveern85u2.apps.googleusercontent.com',
   clientSecret: 'bo4y8hHxESvUcdLVKz8JQTuZ',
-  redirect: 'http://localhost:1337/auth/google/callback'
+  redirect: 'http://www.vrdnetwork.com:1337/auth/google/callback'
 };
 const defaultScope = [
   'https://www.googleapis.com/auth/plus.me',
