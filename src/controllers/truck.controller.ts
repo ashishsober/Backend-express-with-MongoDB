c7fee396ee6947157d37d319bcc578fbb4dce0f0
Ashish <agupt255>
2020-07-16 17:50:15 +0530

    Update truck.controller.ts

--- src/controllers/truck.controller.ts
+++ src/controllers/truck.controller.ts
@@ -43,7 +43,9 @@ export class TruckController {
         }
         const data = { $push: { "ownerOf": {
                 truck_id:truckData._id,
-                truck_no:truckData.truck_no
+                truck_no:truckData.truck_no,
+                driver_name:truckData.driver_name,
+                driver_emailId:truckData.driver_emailId,
             }
          } 
         }
