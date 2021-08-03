44345c6e50770d427b8360dc4027d8faa97cbe38
Gupta, Ashish <gupta.ashish@optum.com>
2021-08-03 14:35:26 +0530

    Update auth.ts

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,5 +2,6 @@ export enum DBURL {
 	URL = "mongodb://localhost:27017/data",
 	PROD_URL = "mongodb+srv://test:test123@truckbypass.g6x5m.mongodb.net/truckbypass?retryWrites=true&w=majority",
 	dockerMongoExpress = "mongodb://admin:password@mongodb:27017/admin",
+	replicaset = "mongodb://mongo1:27017,mongo2:27018,mongo3:27019/test?replicaSet=my-mongo-set",
 	toRundockerMongoExpressLocally = "mongodb://admin:password@localhost:27017/admin"
 }
